09c428cc99d24e741007f4167e8060fcbebce416
shantosh <shantosh25@hotmail.com>
2023-04-14 07:23:14 +0530

    commiting all the code again

--- src/test/java/com/acti/testcase/BaseTest.java
+++ src/test/java/com/acti/testcase/BaseTest.java
@@ -1,20 +1,24 @@
 package com.acti.testcase;
 
+import java.io.IOException;
+
+import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
-
 import com.acti.driver.DriverManager;
 import com.acti.pages.LoginPage;
+import com.acti.utils.Helper;
 import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.MediaEntityBuilder;
 import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
 
 public class BaseTest extends DriverManager {
 	
 	protected static ExtentHtmlReporter extent;
 	protected static ExtentReports report;
-	protected static ExtentTest logger;
+	static ExtentTest logger;
 	LoginPage lp;
 	
 	@BeforeSuite
@@ -31,9 +35,28 @@ public class BaseTest extends DriverManager {
 	}
 	
 	@AfterMethod
-	public void tearDown() {
+	public void tearDown(ITestResult result) 
+	{
+		if(result.getStatus()==ITestResult.FAILURE)
+		{
+			try {
+				logger.fail("Test Failed",
+						MediaEntityBuilder.createScreenCaptureFromPath(Helper.captureScreen(driver)).build());
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
 		report.flush();
 		quitBrowser();
 	}
 
 }
+
+
+
+
+
+
+
+
